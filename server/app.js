6e9b9c10b9a4f3936a3aa218993bded7c033c2a2
gorguero <jcaguerodev@gmail.com>
2023-06-15 18:23:33 -0300

    Primeras configuraciones del servidor

--- server/app.js
+++ server/app.js
@@ -1,1 +1,15 @@
-console.log('Hola desde back de kondur')
+const express = require('express');
+const port = 5000;
+const app = express();
+
+//Rutas
+app.get('/', (req, res) => {
+    res.json({
+        ok: true,
+        msg: 'Conectado a Kondur Backend'
+    })
+})
+
+app.listen(port, () => {
+    console.log(`Puerto ${port} en marcha`);
+})
