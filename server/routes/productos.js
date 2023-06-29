09d77608ce46350374468d3954919ac1a1a26ffe
gorguero <jcaguerodev@gmail.com>
2023-06-29 17:31:19 -0300

    Validaciones para productos

--- server/routes/productos.js
+++ server/routes/productos.js
@@ -1,14 +1,24 @@
 import express from "express";
 import { check } from "express-validator";
 import { createProducto, getProducto, getProductos,updateProducto,deleteProducto } from "../controllers/productos.js";
-import { isProductNameExist } from "../helpers/db-validations.js";
+import { isProductNameExist, isDescriptionProductExist, isValueMin, isProductExistById } from "../helpers/db-validations.js";
 import { validarCampos } from "../middlewares/validaciones.js";
 
 const router = express.Router();
 
 //Crear Producto
 router.post('/',[
+    check('nombreProducto', 'El nombre del producto es obligatorio.').not().isEmpty(),
     check('nombreProducto').custom(isProductNameExist),
+    check('descripcion', 'La descripción del producto es requerida.').not().isEmpty(),
+    check('descripcion').custom(isDescriptionProductExist),
+    check('descripcion', 'Debe contener solamente 250 caracteres.').isLength({max: 250}),
+    check('imagen', 'La url de la imagen es requerida.').not().isEmpty(),
+    check('precio', 'El valor del precio es requerido.').not().isEmpty(),
+    check('precio').custom(isValueMin),
+    check('stock', 'El valor del stock es requerido.').not().isEmpty(),
+    check('stock').custom(isValueMin),
+    check('categoria', 'La categoria es requerida.').not().isEmpty(),
     validarCampos
 ],createProducto);
 
@@ -17,13 +27,35 @@ router.get('/', getProductos);
 
 //Actualizar Producto
 router.put('/:id',[
+    check('id', 'No es un ID válido').isMongoId(),
+    check('id').custom( isProductExistById ),
     check('nombreProducto').custom(isProductNameExist),
+    check('nombreProducto', 'El nombre del producto es obligatorio.').not().isEmpty(),
+    check('nombreProducto').custom(isProductNameExist),
+    check('descripcion', 'La descripción del producto es requerida.').not().isEmpty(),
+    check('descripcion').custom(isDescriptionProductExist),
+    check('descripcion', 'Debe contener solamente 250 caracteres.').isLength({max: 250}),
+    check('imagen', 'La url de la imagen es requerida.').not().isEmpty(),
+    check('precio', 'El valor del precio es requerido.').not().isEmpty(),
+    check('precio').custom(isValueMin),
+    check('stock', 'El valor del stock es requerido.').not().isEmpty(),
+    check('stock').custom(isValueMin),
+    check('categoria', 'La categoria es requerida.').not().isEmpty(),
+    validarCampos
 ],updateProducto);
 
 //Buscar un Producto
-router.get('/:id', getProducto);
+router.get('/:id',[
+    check('id', 'No es un ID válido').isMongoId(),
+    check('id').custom( isProductExistById ),
+    validarCampos
+], getProducto);
 
 //Borrado logico de un Producto
-router.delete('/:id', deleteProducto);
+router.delete('/:id',[
+    check('id', 'No es un ID válido').isMongoId(),
+    check('id').custom( isProductExistById ),
+    validarCampos
+], deleteProducto);
 
 export default router;
