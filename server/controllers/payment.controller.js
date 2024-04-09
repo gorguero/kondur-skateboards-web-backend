024aac518149b11d80a7d4b691cd703f2c38c445
Marcos Quinteros <104270568+M4gnus99@users.noreply.github.com>
2024-04-09 13:04:01 -0300

    cambios en los items que recibe la api
    se cambio la manera en la que se cargan los items en la peticion a la api

--- server/controllers/payment.controller.js
+++ server/controllers/payment.controller.js
@@ -11,20 +11,21 @@ const preference = new Preference(client);
 const createOrder = async (req, res) => {
   try {
     console.log('Preferencia antes de la solicitud a MercadoPago:', preference);
-    const product = req.body;
-    
+    const products = req.body.productos;
+    console.log(products);
+
+    // Crear un array de items para la preferencia de MercadoPago
+    const items = products.map(producto => ({
+      title: producto.nombreProducto,
+      currency_id: 'ARS',
+      description: producto.descripcion,
+      picture_url: producto.imagen,
+      unit_price: Number(producto.precio),
+      quantity: Number(producto.cantidad),
+    }));
     const result = await  preference.create(
       {body: {
-        items: [
-          {
-            title: product.nombreProducto,
-            currency_id: 'ARS',
-            description: product.descripcion,
-            picture_url: product.imagen,
-            unit_price: Number(product.precio),
-            quantity: Number(product.cantidad),
-          }
-        ],
+        items: items,
         back_urls: {
           success: 'https://localhost:4200',
           failure: 'http://www.failure.com',
